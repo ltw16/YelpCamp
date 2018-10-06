49a5292a033b9a60fbac6f6d64effbead7f3cb46
Liam Webb <liamwebb11@hotmail.com>
2018-10-06 14:29:48 +0000

    Added main CSS stylesheet and edited show campgrounds page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose
     
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function(req, res){
