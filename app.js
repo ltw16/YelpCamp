604a2fab6250085d5148f3e255cb44355e2059e3
Liam Webb <liamwebb11@hotmail.com>
2018-10-07 16:34:38 +0000

    First deploy

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
     passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
     LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
+    flash        = require("connect-flash"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds")
+    session = require("express-session"),
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override");
     
 //requiring routes
 var commentRoutes    = require("./routes/comments"),
@@ -25,7 +27,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(flash());
+app.use(cookieParser('secret'));
+
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
@@ -34,6 +37,8 @@ app.use(require("express-session")({
     resave: false,
     saveUninitialized: false
 }));
+
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -42,15 +47,15 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
    next();
 });
+
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
