f6092641d181b2dcb25b8aed2f7ec7899088dcc0
Liam Webb <liamwebb11@hotmail.com>
2018-10-07 11:29:42 +0000

    Added author to campground

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
